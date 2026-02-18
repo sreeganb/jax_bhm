8806b00365bab0c2afcc756277af8888286da8dd
Sree Ganesh Balasubramani <sreeganeshbalasubramani@Srees-MacBook-Pro-6.local>
2026-02-18 11:40:50 -0800

    update

--- sampling/__init__.py
+++ sampling/__init__.py
@@ -12,12 +12,6 @@ from .smc import (
     get_smc_samples,
     get_best_sample,
 )
-from .rigid_body_proposal import (
-    create_rigid_body_rmh_kernel,
-    create_rigid_body_smc_step_fn,
-    run_rigid_body_rmh,
-    run_rigid_body_smc,
-)
 
 __all__ = [
     'create_rmh_kernel',
@@ -26,9 +20,5 @@ __all__ = [
     'run_parallel_rmh',
     'run_tempered_smc',
     'get_smc_samples',
-    'get_best_sample',
-    'create_rigid_body_rmh_kernel',
-    'create_rigid_body_smc_step_fn',
-    'run_rigid_body_rmh',
-    'run_rigid_body_smc',
+    'get_best_sample'
 ]
